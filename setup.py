bf62002660156f0121649c8ddc4cbb038aefad32
Ulrich Armel <mulricharmel@gmail.com>
2022-03-27 17:20:08 +0200

    added description

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ def requirements():
         return [pname.strip() for pname in f.readlines()]
 
 
-# with open("README.rst") as tmp:
-#     readme = tmp.read()
+with open("README.rst") as tmp:
+    readme = tmp.read()
 
 console_scripts = ['pulsar-rime=rime.main:main', 'pulsar-beam=rime.beam_variation:main']
 
@@ -21,8 +21,8 @@ setup(
     name='pulsar-rime',
     version=__version__,
     description='Computing visibilities for variable source with a beamgain using jax',
-    # long_description=readme,
-    # long_description_content_type="text/x-rst",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
     url='https://github.com/ulricharmel/pulsar-rime',
     license='GNU GPL v2',
     install_requires=requirements(),
